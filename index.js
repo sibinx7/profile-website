3900f16a84f8135387e075dfaace1f7654839720
Sibin Xavier <sibinx7@gmail.com>
2017-08-02 23:08:10 +0530

    Add inert for static content serve

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = new Hapi.Server();
 
 server.connection({port: 4000, host: 'localhost'})
 
-server.register(require('vision'), function(){
+server.register([require('inert'), require('vision')], function(){
   server.route({
     method:'GET',
     path: '/',
@@ -13,14 +13,37 @@ server.register(require('vision'), function(){
     }
   });
 
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'dist',
+        listing: true
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/assets/{param*}',
+    handler: {
+      directory: {
+        path: 'dist',
+        listing: true
+      }
+    }
+  });
+
   server.views({
       engines: {
           html: require('handlebars')
       },
       relativeTo: __dirname,
-      path: 'src'
+      path: 'dist'
   });
 });
+
 
 server.start(function(err){
   console.log("Server loading...")
