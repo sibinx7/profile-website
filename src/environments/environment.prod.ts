4d0afae29501e0cd2d1c3df0c56b229516c39fed
Sibin Xavier <sibinx7@gmail.com>
2017-12-19 16:15:36 +0530

    Show only 6 posts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-    BLOG_API_URL: `http://blog.7chip.com/wp-json/wp/v2/posts?per_page=12`
+    BLOG_API_URL: `http://blog.7chip.com/wp-json/wp/v2/posts?per_page=6`
 };
