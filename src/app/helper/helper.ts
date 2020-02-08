73c7c0980a9817ed4feaca9f52db070e4afd37ed
Sibin Xavier <sibinx7@gmail.com>
2020-02-08 13:31:47 +0530

    Fix issues in type script type

--- src/app/helper/helper.ts
+++ src/app/helper/helper.ts
@@ -2,12 +2,12 @@
  * @method
  * @summary Calculate year difference between 2 date
  * @description Calculate year difference between 2 date
- * @param Date date Date
+ * @param date date Date
  */
 export const yearDiffFromNow = (date: Date): number => {
-	const currentDate = new Date();
-	const inputDate = new Date(date);
-	const diffTime: number = Math.abs(currentDate - inputDate);
+	const currentDate: Date = new Date();
+	const inputDate: Date = new Date(date);
+	const diffTime: number = Math.abs(currentDate.getTime() - inputDate.getTime());
 	const diffYears: number = Math.ceil( (diffTime) / (1000 * 60 * 60 * 24 * 365));
 	return Math.ceil(diffYears);
 };
